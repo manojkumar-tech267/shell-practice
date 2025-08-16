9bb040b4d4f79af670b162fada71f14aeb468927
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2025-08-16 12:30:46 +0530

    Made some changes

--- 12-install-nginx.sh
+++ 12-install-nginx.sh
@@ -1,4 +1,11 @@
 #!/bin/bash 
 
-userid=$(id -u) 
-echo $userid 
+userid=$(id -u)
+
+if [ userid -ne 0 ] 
+then 
+    echo "Please!!! run with root user"
+    exit 1 
+else 
+    echo "You are running with root user access"
+echo $? 
