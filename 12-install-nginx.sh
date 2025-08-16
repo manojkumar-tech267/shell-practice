42e3f50d09444bf14e7936098f72a1a74162e75e
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2025-08-16 12:36:23 +0530

    Made some changes

--- 12-install-nginx.sh
+++ 12-install-nginx.sh
@@ -9,4 +9,21 @@ then
 else 
     echo "You are running with root user access"
 fi 
-echo $? 
+
+dnf list installed nginx
+
+if [ $? -ne 0 ] 
+then 
+    echo "Nginx is not installed we are installing!!!"
+    dnf install nginx -y 
+    if [ $? -eq 0 ]
+    then 
+        echo "Nginx is installed successfully" 
+    else 
+        echo "Nginx is not installed successfully"
+        exit 1
+    fi 
+else 
+    echo "Nginx is already installed we are nothing to do!!!"
+fi 
+
