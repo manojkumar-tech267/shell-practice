fa93c6e3150c22bc0d3286155ff02fc3cc2f9934
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2025-08-16 09:20:27 +0530

    Conversation file added

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+echo "Hi Manoj!!"
+echo "Hello siva Thanku for asking about me"
+echo "How is your devops Learning Manoj"
+echo "Yeah siva going good your teaching style is very easy"
+echo "Thank you Manoj Thanks for your Feedback!!!"
+echo "All the best for your future"
