ad55aab970211267515a27d4ee38a455acbc6569
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2025-08-16 11:10:40 +0530

    Modifications done

--- 11-install-command.sh
+++ 11-install-command.sh
@@ -1,10 +1,13 @@
 #!/bin/bash 
-userId=$(id -u)
 
-if [ userId -ne 0 ]
+userid=$(id -u)
+
+if [ userid -ne 0 ]
 then 
-    echo "Error:: Please run this script with root access"
-    exit 1
+    echo "Please!!! run with root access"
+    exit 1 
 else 
     echo "You are running with root access"
 fi 
+
+echo $? 
