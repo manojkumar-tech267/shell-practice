3275531395fc8a8e2be4bdb737e45b8bf3ea9e28
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2025-08-16 09:54:31 +0530

    New files added

---
+++ 07-datatypes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+# This line prints current date and time 
+timestamp=$(date) 
+
+Number1=100
+Number2=200
+
+echo "Today's date is ${timestamp}"   
+echo "Addition is $((Number1+Number2))"
---
+++ 08-arrays.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+Movies=("Ragada" "Coolie" "War2" "Mass Jathara")
+
+echo "${Movies[0]}"
+echo "${Movies[1]}"
+echo "${Movies[@]}"
