6ef7a89a480d230b1e9d7985cb5c011439017f14
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2025-08-16 09:34:46 +0530

    added new file

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+Person1=$1
+Person2=$2
+
+
+echo "Hi ${Person1}!! How are you?"
+echo "Hello ${Person2} Thanku for asking about me"
+echo "How is your devops Learning ${Person1}"
+echo "Yeah ${Person2} going good your teaching style is very easy"
+echo "Thank you ${Person1} Thanks for your Feedback!!!"
+echo "All the best for your future"
+echo "Thank you ${Person2}"
