2aca993ecee457051271f4fe551605e8c6e22660
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2025-08-16 09:22:04 +0530

    conversation file modified

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
-echo "Hi Manoj!!"
+echo "Hi Manoj!! How are you?"
 echo "Hello siva Thanku for asking about me"
 echo "How is your devops Learning Manoj"
 echo "Yeah siva going good your teaching style is very easy"
 echo "Thank you Manoj Thanks for your Feedback!!!"
 echo "All the best for your future"
+echo "Thank you Siva"
