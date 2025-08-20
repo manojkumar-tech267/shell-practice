a77cf2883f81be66b775972c2c3bb1a3e3498441
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2025-08-21 00:09:55 +0530

    We modifed some files

--- 12-install-nginx.sh
+++ 12-install-nginx.sh
@@ -2,28 +2,27 @@
 
 userid=$(id -u)
 
-if [ $userid -ne 0 ] 
+if [ $userid -ne 0 ]
 then 
-    echo "Please!!! run with root user"
+    echo "Please!!! run with root access you are not a root user"
     exit 1 
 else 
-    echo "You are running with root user access"
+    echo "You are a root user!!"
 fi 
 
-dnf list installed nginx
+dnf list installed nginx 
 
-if [ $? -ne 0 ] 
+if [ $? -ne 0 ]
 then 
-    echo "Nginx is not installed we are installing!!!"
+    echo "Nginx is not installed we are going to install!!!"
     dnf install nginx -y 
     if [ $? -eq 0 ]
     then 
-        echo "Nginx is installed successfully" 
+        echo "Nginx installed successfully"
     else 
-        echo "Nginx is not installed successfully"
-        exit 1
+        echo "Nginx is not installed successfully!!!"
+        exit 1 
     fi 
 else 
-    echo "Nginx is already installed we are nothing to do!!!"
+    echo "Nginx is already installed we are skipping!!!!"
 fi 
-
