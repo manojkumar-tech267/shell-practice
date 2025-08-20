6a808e668a9c9cd0fa4ffa71d83aa3407b946d50
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2025-08-21 00:41:29 +0530

    I added extra statements and colors

--- 14-colors.sh
+++ 14-colors.sh
@@ -7,6 +7,7 @@ Y="\e[33m"
 echo -e "$Y Hello World $N"
 echo -e "$G Hiii Manoj $N"
 echo -e "$G I am learning devops from joindevops It is excellent platform to learn devops $N"
+echo -e "$R I get procastination when open the laptop it is not good habit $N"
 
 
 
