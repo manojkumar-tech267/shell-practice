9b4878c5be45efac4ed2e580ac2de59f0512f8d4
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2025-08-21 10:11:33 +0530

    improved echo statements

--- 15-colors.sh
+++ 15-colors.sh
@@ -8,19 +8,19 @@ N="\e[0m"
 
 if [ $userid -ne 0 ]
 then 
-    echo -e "$R You are not a root user run with root access $N"
+    echo -e "$R Error: This script must be run as root. Please use sudo or switch to the root user. $N"
     exit 1 
 else 
-    echo -e "$G you are a root user $N"
+    echo -e "$G Root access confirmed. Proceeding with installation checks. $N"
 fi 
 
 VALIDATE()
 {
     if [ $1 -eq 0 ]
     then 
-        echo -e "$G $2 installed successfully $N"
+        echo -e "$G Success: $2 has been installed successfully. $N"
     else 
-        echo -e "$R $2 not installed successfully $N" 
+        echo -e "$R Error: $2 installation failed. Please check your package manager and network connection. $N"
         exit 1 
     fi 
 }
@@ -28,19 +28,19 @@ VALIDATE()
 dnf list installed mysql
 if [ $? -ne 0 ]
 then 
-    echo -e "$R Mysql is not there in our system we are going to install $N"
+    echo -e "$Y MySQL is not installed. Starting installation now. $N"
     dnf install mysql -y 
     VALIDATE $? "MySQL" 
 else 
-    echo -e "$Y MySQL is already there we are going to skip!!! $N"
+    echo -e "$G MySQL is already installed. No action needed. $N"
 fi 
 
 dnf list installed nginx 
 if [ $? -ne 0 ]
 then 
-    echo -e "$R Nginx is not there in our system we are going to install!!! $N"
+    echo -e "$Y Nginx is not installed. Starting installation now. $N"
     dnf install nginx -y 
     VALIDATE $? "Nginx" 
 else 
-    echo -e "$Y Nginx is already there we are going to skip!!! $N" 
+    echo -e "$G Nginx is already installed. No action needed. $N" 
 fi 
